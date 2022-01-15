8242d793c85e0c3175c6573ed42ec5d816118632
Ruslan Egorov <r-egorov@outlook.com>
2022-01-15 16:23:24 +0300

    Add lexicographical_compare

--- includes/ft_utilities.hpp
+++ includes/ft_utilities.hpp
@@ -15,47 +15,39 @@ struct enable_if<true, T> { typedef T type; };
 /*
 ** is_integral implementation
 */ 
-template <class T, T v>
-struct integral_constant {
-    static constexpr T          value = v;
-    typedef                     value_type;
-    typedef                     integral_constant<T,v> type;
 
-    constexpr   operator T() { return v; }
-};
-
-typedef ft::integral_constant<bool, true>   true_type;
-typedef ft::integral_constant<bool, false>  false_type;
+typedef std::integral_constant<bool, true>   true_type;
+typedef std::integral_constant<bool, false>  false_type;
 
 template<typename> struct is_integral : ft::false_type {};
 template<> struct is_integral<bool> : ft::true_type {};
-template<> struct is_integral<short> : ft::true_type {};
 template<> struct is_integral<char> : ft::true_type {};
-template<> struct is_integral<int> : ft::true_type {};
-template<> struct is_integral<long> : ft::true_type {};
-template<> struct is_integral<long long> : ft::true_type {};
-template<> struct is_integral<unsigned short> : ft::true_type {};
-template<> struct is_integral<unsigned char> : ft::true_type {};
-template<> struct is_integral<unsigned int> : ft::true_type {};
-template<> struct is_integral<unsigned long> : ft::true_type {};
-template<> struct is_integral<unsigned long long> : ft::true_type {};
 template<> struct is_integral<char16_t> : ft::true_type {};
 template<> struct is_integral<char32_t> : ft::true_type {};
 template<> struct is_integral<wchar_t> : ft::true_type {};
+template<> struct is_integral<short> : ft::true_type {};
+template<> struct is_integral<int> : ft::true_type {};
+template<> struct is_integral<long> : ft::true_type {};
+template<> struct is_integral<long long> : ft::true_type {};
+template<> struct is_integral<unsigned char> : ft::true_type {};
+template<> struct is_integral<unsigned short> : ft::true_type {};
+template<> struct is_integral<unsigned int> : ft::true_type {};
+template<> struct is_integral<unsigned long> : ft::true_type {};
+template<> struct is_integral<unsigned long long> : ft::true_type {};
 template<> struct is_integral<const bool> : ft::true_type {};
-template<> struct is_integral<const short> : ft::true_type {};
 template<> struct is_integral<const char> : ft::true_type {};
-template<> struct is_integral<const int> : ft::true_type {};
-template<> struct is_integral<const long> : ft::true_type {};
-template<> struct is_integral<const long long> : ft::true_type {};
-template<> struct is_integral<const unsigned short> : ft::true_type {};
-template<> struct is_integral<const unsigned char> : ft::true_type {};
-template<> struct is_integral<const unsigned int> : ft::true_type {};
-template<> struct is_integral<const unsigned long> : ft::true_type {};
-template<> struct is_integral<const unsigned long long> : ft::true_type {};
 template<> struct is_integral<const char16_t> : ft::true_type {};
 template<> struct is_integral<const char32_t> : ft::true_type {};
 template<> struct is_integral<const wchar_t> : ft::true_type {};
+template<> struct is_integral<const short> : ft::true_type {};
+template<> struct is_integral<const int> : ft::true_type {};
+template<> struct is_integral<const long> : ft::true_type {};
+template<> struct is_integral<const long long> : ft::true_type {};
+template<> struct is_integral<const unsigned char> : ft::true_type {};
+template<> struct is_integral<const unsigned short> : ft::true_type {};
+template<> struct is_integral<const unsigned int> : ft::true_type {};
+template<> struct is_integral<const unsigned long> : ft::true_type {};
+template<> struct is_integral<const unsigned long long> : ft::true_type {};
 
 
 /*
@@ -74,13 +66,39 @@ bool equal(InputIterator1 first1, InputIterator1 last1,
 
 template <class InputIterator1, class InputIterator2, class BinaryPredicate>
 bool equal (InputIterator1 first1, InputIterator1 last1,
-            InputIterator2 first2, BinaryPredicate pred); {
+            InputIterator2 first2, BinaryPredicate pred) {
     while (first1 != last1) {
         if (!pred(*first1,*first2))
             return (false);
         ++first1; ++first2;
     }
   return true;
+}
+
+/*
+** lexicographical_compare implementation
+*/
+template <class InputIterator1, class InputIterator2>
+bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+                              InputIterator2 first2, InputIterator2 last2)
+{
+    for ( ; (first1 != last1) && (first2 != last2); ++first1, (void)++first2 ) {
+        if (*first1 < *first2) return (true);
+        if (*first2 < *first1) return (false);
+    }
+    return (first1 == last1) && (first2 != last2);
+}
+
+template <class InputIterator1, class InputIterator2, class Compare>
+bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+                              InputIterator2 first2, InputIterator2 last2,
+                              Compare comp)
+{
+    for ( ; (first1 != last1) && (first2 != last2); ++first1, (void)++first2 ) {
+        if (comp(*first1, *first2)) return (true);
+        if (comp(*first2, *first1)) return (false);
+    }
+    return (first1 == last1) && (first2 != last2);
 }
 
 } // namespace brace
